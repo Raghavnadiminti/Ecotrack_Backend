eaf481bfb27156e30b43f94d4591b711972b14ac
Raghavnadiminti <raghavnadiminti@gmail.com>
2024-12-30 21:21:03 +0530

    registers ang logins

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors=require('cors')
 const {request_locations,server,app}=require('./Locations')
 const socket=require('socket.io')
 const http=require('http')
-
+const {locations,user_login_coll,admin_login_coll,collector_login_coll}=require('./models')
 
 
 
@@ -22,7 +22,81 @@ app.post('/users-request',async (req,res)=>{
         res.send({near:near_user}) 
 })
 
+app.post('/user-login',async (req,res)=>{
+
+          const {username,password}=req.query;
+          let k = await user_login_coll.findOne({username:username,password:password}) 
+          if(k){
+                res.send(true)
+          }
+          else{
+                res.send(false)
+          }
+})
+app.post('/admin-login',async (req,res)=>{
+
+        const {id,username,password}=req.body;
+        let k = await admin_login_coll.findOne({id:id,username:username,password:password}) 
+        if(k){
+              res.send(true)
+        }
+        else{
+              res.send(false)
+        }
+})
+app.post('/collector-login',async (req,res)=>{
+
+        const {id,username,password}=req.body;
+        let k = await collector_login_coll.findOne({id:id,username:username,password:password}) 
+        if(k){
+              res.send(true)
+        }
+        else{
+              res.send(false)
+        }
+})
+app.post('/user-register',async (req,res)=>{
+        const {email,username,password}=req.query;
+        let k = await user_login_coll.findOne({username:username}) 
+        if(k){
+              res.send(false)
+        }
+        else{
+                let user=new user_login_coll({email:email,username:username,password:password})
+                await user.save();
+              
+                res.send(true)
+        }
 
 
+})
+app.post('/collector-register',async (req,res)=>{
+        const {email,username,password,id}=req.body;
+        let k = await collector_login_coll.findOne({username:username}) 
+        if(k){
+              res.send(false)
+        }
+        else{
+                let collector=new collector_login_coll({email:email,id:id,username:username,password:password})
+                await collector.save();
+              
+                res.send(true)
+        }
+
+
+})
+app.post('/admin-register',async (req,res)=>{
+        const {email,username,password}=req.body;
+        let k = await admin_login_coll.findOne({username:username}) 
+        if(k){
+              res.send(false)
+        }
+        else{
+                let admin=new admin_login_coll({email:email,username:username,password:password})
+                await admin.save();
+              
+                res.send(true)
+        }
+})
 
 server.listen(5000,()=>{console.log("listening")})
--- models.js
+++ models.js
@@ -8,13 +8,35 @@ const collectors_scheema=mongoose.Schema({
     lon:String
 })
 
-const scheema=mongoose.Schema({
+const location_scheema=mongoose.Schema({
       City:String,
       collectors:[collectors_scheema]
       
 })
 
-const locations=mongoose.model('location',scheema) 
+const collector_login_scheema=mongoose.Schema({
+    id:String,
+    username:String,
+    password:String
+})
 
-module.exports={locations}
+const user_login_scheema=mongoose.Schema({
+    email:String,
+    username:String,
+    password:String,
+})
+
+const admin_login_scheema=mongoose.Schema({
+    id:String,
+    username:String,
+    password:String
+})
+
+const user_login_coll=mongoose.model('user_login',user_login_scheema)
+const admin_login_coll=mongoose.model('admin_login',admin_login_scheema)
+const collector_login_coll=mongoose.model('collector_login',collector_login_scheema)
+
+const locations=mongoose.model('location',location_scheema) 
+
+module.exports={locations,user_login_coll,admin_login_coll,collector_login_coll}
 
